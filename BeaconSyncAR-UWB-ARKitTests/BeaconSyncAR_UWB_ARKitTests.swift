a9a9f995452929316cb91b47810ccde66df1fef5
Matthew <52621525+H11Maitree@users.noreply.github.com>
2025-03-13 16:25:24 +0800

    Initial Commit

---
+++ BeaconSyncAR-UWB-ARKitTests/BeaconSyncAR_UWB_ARKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconSyncAR_UWB_ARKitTests.swift
+//  BeaconSyncAR-UWB-ARKitTests
+//
+//  Created by Maitree Hirunteeyakul on 13/3/2025.
+//
+
+import Testing
+@testable import BeaconSyncAR_UWB_ARKit
+
+struct BeaconSyncAR_UWB_ARKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
